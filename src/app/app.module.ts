80cb89870a9269ce1111928e80cef0d2221244c9
muriloanjos99 <muriloanjos99@gmail.com>
2023-10-17 17:56:47 -0300

    Componentes criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { PageTitleComponent } from './page-title/page-title.component';
+import { MainCardComponent } from './main-card/main-card.component';
+import { NewsSidebarComponent } from './news-sidebar/news-sidebar.component';
+import { SideCardsComponent } from './news-sidebar/side-cards/side-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    PageTitleComponent,
+    MainCardComponent,
+    NewsSidebarComponent,
+    SideCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/news-sidebar/side-cards/side-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-cards',
+  templateUrl: './side-cards.component.html',
+  styleUrls: ['./side-cards.component.css']
+})
+export class SideCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
