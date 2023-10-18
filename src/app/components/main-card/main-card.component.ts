eeb89ca399b5dd8fe9ee2ce3a8983a30e7f8b0c3
muriloanjos99 <muriloanjos99@gmail.com>
2023-10-18 16:16:37 -0300

    Componentes transformados em dinâmicos

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card.component.css']
 })
 export class MainCardComponent implements OnInit {
+  @Input()
+  photoCover: string = "";
+  @Input()
+  cardTitle: string = "";
+  @Input()
+  cardDescription: string = "";
 
   constructor() { }
 
--- src/app/components/news-sidebar/side-card/side-card.component.ts
+++ src/app/components/news-sidebar/side-card/side-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-card.component.css']
 })
 export class SideCardComponent implements OnInit {
+  @Input()
+  photoCover: string = "";
+  @Input()
+  cardTitle: string = "";
 
   constructor() { }
 
